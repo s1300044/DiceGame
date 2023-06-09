bae9fc93a9e18dff176952df61d20d945ab71ba9
s1300044 <136040328+s1300044@users.noreply.github.com>
2023-06-09 12:27:56 +0900

    DiceGame.c

--- DiceGame.c
+++ DiceGame.c
@@ -3,10 +3,7 @@ include<stdio.h>
 include<stdlib.h>
 int main(){
 int sum=0;
-char name[10];
-printf("What is your name?\n");
-scanf("%s",&name);
-printf("Hello, %s!\nRolling dice...\n",name);
+printf("Rolling dice...\n");
 for(int i=1;i<3;i++){
 int n;
 n=rand()%6+1;
@@ -14,5 +11,4 @@ printf("Die %d:%d\n",i,n);
 sum+=n;
 }
 printf("Total value:%d\n",sum);
-printf("%s won!\n",name);
 }
